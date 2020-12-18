2af7bf324a0a6badb1d9a2da9c748f75623ed60e
Brandon Beck <beckbran@gmail.com>
2020-12-18 10:34:57 -0600

    debug

--- vsphere/resource_vsphere_virtual_machine.go
+++ vsphere/resource_vsphere_virtual_machine.go
@@ -1556,86 +1556,86 @@ func resourceVSphereVirtualMachineCreateClone(d *schema.ResourceData, meta inter
 func resourceVSphereVirtualMachinePostDeployChanges(d *schema.ResourceData, meta interface{}, vm *object.VirtualMachine) error {
 	client := meta.(*VSphereClient).vimClient
 	poolID := d.Get("resource_pool_id").(string)
-	_, err := resourcepool.FromID(client, poolID)
-	vprops, err := virtualmachine.Properties(vm)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("cannot fetch properties of created virtual machine: %s", err),
-		)
-	}
-	log.Printf("[DEBUG] VM %q - UUID is %q", vm.InventoryPath, vprops.Config.Uuid)
-	d.SetId(vprops.Config.Uuid)
+	_, _ = resourcepool.FromID(client, poolID)
+	// vprops, err := virtualmachine.Properties(vm)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("cannot fetch properties of created virtual machine: %s", err),
+	// 	)
+	// }
+	// log.Printf("[DEBUG] VM %q - UUID is %q", vm.InventoryPath, vprops.Config.Uuid)
+	// d.SetId(vprops.Config.Uuid)
 
-	// Before starting or proceeding any further, we need to normalize the
-	// configuration of the newly cloned VM. This is basically a subset of update
-	// with the stipulation that there is currently no state to help move this
-	// along.
-	cfgSpec, err := expandVirtualMachineConfigSpec(d, client)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("error in virtual machine configuration: %s", err),
-		)
-	}
+	// // Before starting or proceeding any further, we need to normalize the
+	// // configuration of the newly cloned VM. This is basically a subset of update
+	// // with the stipulation that there is currently no state to help move this
+	// // along.
+	// cfgSpec, err := expandVirtualMachineConfigSpec(d, client)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("error in virtual machine configuration: %s", err),
+	// 	)
+	// }
 
-	// To apply device changes, we need the current devicecfgSpec from the config
-	// info. We then filter this list through the same apply process we did for
-	// create, which will apply the changes in an incremental fashion.
-	devices := object.VirtualDeviceList(vprops.Config.Hardware.Device)
-	var delta []types.BaseVirtualDeviceConfigSpec
-	// First check the state of our SCSI bus. Normalize it if we need to.
-	devices, delta, err = virtualdevice.NormalizeBus(devices, d)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("error normalizing SCSI bus post-clone: %s", err),
-		)
-	}
-	cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
-	// Disks
-	devices, delta, err = virtualdevice.DiskPostCloneOperation(d, client, devices)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("error processing disk changes post-clone: %s", err),
-		)
-	}
-	cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
-	// Network devices
-	devices, delta, err = virtualdevice.NetworkInterfacePostCloneOperation(d, client, devices)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("error processing network device changes post-clone: %s", err),
-		)
-	}
-	cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
-	// CDROM
-	devices, delta, err = virtualdevice.CdromPostCloneOperation(d, client, devices)
-	if err != nil {
-		return resourceVSphereVirtualMachineRollbackCreate(
-			d,
-			meta,
-			vm,
-			fmt.Errorf("error processing CDROM device changes post-clone: %s", err),
-		)
-	}
+	// // To apply device changes, we need the current devicecfgSpec from the config
+	// // info. We then filter this list through the same apply process we did for
+	// // create, which will apply the changes in an incremental fashion.
+	// devices := object.VirtualDeviceList(vprops.Config.Hardware.Device)
+	// var delta []types.BaseVirtualDeviceConfigSpec
+	// // First check the state of our SCSI bus. Normalize it if we need to.
+	// devices, delta, err = virtualdevice.NormalizeBus(devices, d)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("error normalizing SCSI bus post-clone: %s", err),
+	// 	)
+	// }
+	// cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
+	// // Disks
+	// devices, delta, err = virtualdevice.DiskPostCloneOperation(d, client, devices)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("error processing disk changes post-clone: %s", err),
+	// 	)
+	// }
+	// cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
+	// // Network devices
+	// devices, delta, err = virtualdevice.NetworkInterfacePostCloneOperation(d, client, devices)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("error processing network device changes post-clone: %s", err),
+	// 	)
+	// }
+	// cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
+	// // CDROM
+	// devices, delta, err = virtualdevice.CdromPostCloneOperation(d, client, devices)
+	// if err != nil {
+	// 	return resourceVSphereVirtualMachineRollbackCreate(
+	// 		d,
+	// 		meta,
+	// 		vm,
+	// 		fmt.Errorf("error processing CDROM device changes post-clone: %s", err),
+	// 	)
+	// }
 
-	cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
+	// cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
 
-	log.Printf("[DEBUG] %s: Final device list: %s", resourceVSphereVirtualMachineIDString(d), virtualdevice.DeviceListString(devices))
-	log.Printf("[DEBUG] %s: Final device change cfgSpec: %s", resourceVSphereVirtualMachineIDString(d), virtualdevice.DeviceChangeString(cfgSpec.DeviceChange))
+	// log.Printf("[DEBUG] %s: Final device list: %s", resourceVSphereVirtualMachineIDString(d), virtualdevice.DeviceListString(devices))
+	// log.Printf("[DEBUG] %s: Final device change cfgSpec: %s", resourceVSphereVirtualMachineIDString(d), virtualdevice.DeviceChangeString(cfgSpec.DeviceChange))
 
 	// Perform updates
 	// if _, ok := d.GetOk("datastore_cluster_id"); ok {
